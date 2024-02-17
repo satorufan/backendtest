f7e253b9f76f4cf30c3bcb9a046ed53807991e87
kimminsanggg <rlaalstkd177@gmail.com>
2024-02-17 12:41:01 +0900

    first commit

---
+++ backendtest/Controllers/user.controller.js
@@ -0,0 +1,28 @@
+const User = require("../Models/user")
+const userController = {}
+
+userController.saveUser = async(userName, sid)=>{
+    let user = await User.findOne({ name: userName})
+
+    if (!user){
+        user = new User({
+            name: userName,
+            token: sid,
+            online: true
+        })
+    }
+
+    user.token = sid
+    user.online = true
+
+    await user.save();
+    return user
+}
+
+userController.checkUser = async(sid)=>{
+    const user = await User.findOne({token:sid})
+    if(!user) throw new Error("user not found")
+    return user
+}
+
+module.exports = userController
