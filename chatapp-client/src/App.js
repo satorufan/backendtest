9701ce4b239cce1e88107cb084f8c24a59076b22
kimminsanggg <rlaalstkd177@gmail.com>
2024-02-22 02:50:13 +0900

    sec

---
+++ chatapp-client/src/App.js
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import "./App.css";
+import socket from "./server"
+import { useState } from "react";
+import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
+
+function App() {
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState("")
+  const [messageList,setMessageList] = useState([])
+
+  useEffect(()=>{
+    socket.on('message', (message)=>{
+      setMessageList((prevState) => prevState.concat(message))
+    })
+    askUserName()
+  }, [])
+
+  const askUserName=()=>{
+    const userName = prompt("당신의 이름을 입력하세요")
+
+    socket.emit("login", userName, (res)=>{
+      if (res?.ok) {
+        setUser(res.data)
+      }
+    })
+  }
+  const sendMessage= (event)=>{
+    event.preventDefault()
+    socket.emit("sendMessage", message, (res)=>{
+      console.log("sendMessage res", res)
+    })
+  }
+  return (
+    <div>
+      <div className="App">
+        <MessageContainer messageList={messageList} user={user}/>
+
+        <InputField
+        message={message}
+        setMessage={setMessage}
+        sendMessage={sendMessage}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
